1d954ec08cf8275bc488d90c61e1eeb75bc6663b
arthurborgesfs <arthurbval@gmail.com>
2023-04-07 23:58:42 -0300

    :wrench: chore: Endpoints and status codes

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 
-import { router } from './routes';
 
 const server = express();
 
 
-
-server.use(express.json());
-
-server.use(router);
+server.get('/', (_,res) => {
+    return res.send('Olá, DEV!');
+});
 
 
 export { server };
